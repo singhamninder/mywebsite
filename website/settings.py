0698a64da70915163447dcc301cc956b0e05c0f9
Amninder Singh <amnindersingh13@gmail.com>
2021-10-06 11:36:02 -0700

    django-heroku

--- website/settings.py
+++ website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -152,3 +153,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if os.getcwd() == '/app':
     DEBUG = False
+
+django_heroku.settings(locals())
