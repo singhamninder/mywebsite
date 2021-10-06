7b5263ca5442cdf27c079a22cdd26c5831880b29
Amninder Singh <amnindersingh13@gmail.com>
2021-10-06 01:05:34 -0700

    whitenoise2

--- website/settings.py
+++ website/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
